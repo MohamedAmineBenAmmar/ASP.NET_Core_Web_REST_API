e996a9c8bac6d85e3c7245c89cf50fcc478831f6
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2024-02-18 04:59:18 +0100

    finished many to many relationship

--- Application/Data/ApplicationDBContext.cs
+++ Application/Data/ApplicationDBContext.cs
@@ -22,12 +22,27 @@ namespace Application.Data
         // Linking the tables to our C# models
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
 
         // Configuring the roles to ensure users can login in otherwise we will get
         // an error without configuring the roles
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // Decalring the primary key for the Portfolio table
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
+
+            // Connect to the table
+            builder.Entity<Portfolio>()
+                .HasOne(p => p.AppUser)
+                .WithMany(p => p.Portfolios)
+                .HasForeignKey(p => p.AppUserId);
+
+             builder.Entity<Portfolio>()
+                .HasOne(p => p.Stock)
+                .WithMany(p => p.Portfolios)
+                .HasForeignKey(p => p.StockId);
 
             List<IdentityRole> roles = new List<IdentityRole>{
                 new IdentityRole{Name = "Admin", NormalizedName = "ADMIN"},
--- Application/Models/AppUser.cs
+++ Application/Models/AppUser.cs
@@ -8,6 +8,6 @@ namespace Application.Models
 {
     public class AppUser: IdentityUser
     {
-        
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
     }
 }
